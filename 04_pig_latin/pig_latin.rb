c49176c04a119389648eca8ea191cf60c130b663
borgified <borgified@gmail.com>
2014-02-19 01:01:26 -0800

    ugh

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,36 @@
+def isvowel(c)
+	vowels = ["a","e","i","o","u"]
+	return vowels.include?(c)
+end
+
+
+def translate(x)
+	result=""
+	x.split(' ').each do |s|
+		if isvowel(s.split('').first)
+			s = s + "ay"
+		elsif (!isvowel(s.split('')[0]) and !isvowel(s.split('')[1]) and !isvowel(s.split('')[2]) )
+			a = s.split('')[0]
+			b = s.split('')[1]
+			c = s.split('')[2]
+			s = s + a + b + c + "ay"
+			s[0]=''
+			s[0]=''
+			s[0]=''
+		elsif (!isvowel(s.split('')[0]) and !isvowel(s.split('')[1]))
+			a = s.split('')[0]
+			b = s.split('')[1]
+			s = s + a + b + "ay"
+			s[0]=''
+			s[0]=''
+		else
+			a = s.split('').first
+			s = s + a + "ay"
+			s[0]=''
+		end
+	result = result + s + ' '
+	end
+	result[-1]=''
+	return result
+end
+
