3d90773dc34821611b1954e24ed15c5ffe7dcda4
borgified <borgified@gmail.com>
2014-02-19 23:52:19 -0800

    i learned break

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,10 +4,15 @@ def isvowel(c)
 end
 
 
-def translate(x)
+def translate2(x)
 	result=""
 	x.split(' ').each do |s|
-		if isvowel(s.split('').first)
+
+		if (s.split('')[0] == "q") && (s.split('')[1] == "u")
+			s = s + "quay"
+			s[0]=''
+			s[0]=''
+		elsif isvowel(s.split('').first)
 			s = s + "ay"
 		elsif (!isvowel(s.split('')[0]) and !isvowel(s.split('')[1]) and !isvowel(s.split('')[2]) )
 			a = s.split('')[0]
@@ -28,9 +33,38 @@ def translate(x)
 			s = s + a + "ay"
 			s[0]=''
 		end
-	result = result + s + ' '
+		result = result + s + ' '
 	end
 	result[-1]=''
 	return result
 end
 
+def translate(string)
+	result=""
+	string.split(' ').each do |word|
+		if isvowel(word[0])
+			result = result + word + "ay"
+		else
+			qdetected=0
+			word.split('').each do |letter|
+				if "#{letter}" == "q"
+					qdetected=1
+				end
+				if ((qdetected == 1) && ("#{letter}" == "u"))
+					qdetected=0
+					word = word + "u"
+					word[0]=''
+				elsif !isvowel("#{letter}")
+					word = word + "#{letter}"
+					word[0]=''
+				else
+					break
+				end
+			end
+			result = result + word + "ay"
+		end
+		result = result + ' '
+	end
+	result[-1]=''
+	return result
+end
